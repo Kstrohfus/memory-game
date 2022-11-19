e9369db13bd44fb05717c7fbaa03bd4dddcc8916
Kstrohfus <houston11@me.com>
2022-11-18 20:26:48 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () =>);
+
+//card options
+
+const cardArray = [
+    {
+    name: "blue",
+    img: "img/blue-flower.png"
+
+},
+    {
+    name: "blue",
+    img: "img/blue-flower.png"
+
+},
+
+    {
+    name: "pink",
+    img: "img/pink-flower.png"
+
+},
+
+    {
+    name: "pink",
+    img: "img/pink-flower.png"
+
+},
+
+    {
+    name: "purple",
+    img: "img/purple-flower.png"
+
+},
+
+    {
+    name: "purple",
+    img: "img/blue-flower.png"
+
+},
+
+    {
+    name: "orange",
+    img: "img/orange-flower.png"
+
+},
+    {
+    name: "orange",
+    img: "img/orange-flower.png"
+
+},
+    {
+    name: "green",
+    img: "img/green-flower.png"
+
+},
+    {
+    name: "green",
+    img: "img/green-flower.png"
+
+},
+    {
+    name: "yellow",
+    img: "img/yellow-flower.png"
+
+},
+    {
+    name: "yellow",
+    img: "img/yellow-flower.png"
+
+}
+]
+
+//Creating board
+
+const grid = document.querySelector(.grid);
+
