de44e992f1e1919343f2f220f1db122a1a34c757
Kstrohfus <houston11@me.com>
2022-11-18 23:21:07 -0700

    Code works but buggy

--- js/script.js
+++ js/script.js
@@ -75,32 +75,29 @@ cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.querySelector(".grid")
 const resultDisplay = document.querySelector("#result")
-var cardsChosen = []
-var cardsChosenId = []
-var cardsWon = []
+let cardsChosen = []
+let cardsChosenId = []
+let cardsWon = []
 
 
 //Creating board
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-        var card = document.createElement("img")
+        const card = document.createElement("img")
 
         card.setAttribute("src", "./img/question-mark.png")
         card.setAttribute("data-id", i)
-
-        //card.addEventListener("click", flipCard)
+        card.addEventListener("click", flipCard)
 
         grid.appendChild(card)
     }
 }
 
-createBoard()
-
 //check for match
 function checkForMatch() {
-    var cards = document.querySelectorAll("img")
-    const optionOneId = cardsChosendId[0]
+    let cards = document.querySelectorAll("img")
+    const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
 
     if (cardsChosen[0] === cardsChosen[1]) {
@@ -120,7 +117,7 @@ function checkForMatch() {
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cardArray.length/2)
+    if (cardsWon.length === cardArray.length/2) {
     resultDisplay.textContent = "Congratulations! You did it!"
     }
 }
@@ -128,8 +125,7 @@ function checkForMatch() {
 //flipping card
 
 function flipCard() {
-    var cardId = this.getAttribute("data-id")
-
+    let cardId = this.getAttribute("data-id")
 
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
@@ -139,8 +135,5 @@ function flipCard() {
     }
 }
 
-
-
-
-
+createBoard ()
 });
