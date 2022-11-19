93e0671379f1dec7c771a0fdfc1656eab65a735b
Kstrohfus <houston11@me.com>
2022-11-18 20:47:48 -0700

    Added function to show grid-notworking yet

--- js/script.js
+++ js/script.js
@@ -5,72 +5,87 @@ document.addEventListener("DOMContentLoaded", () =>);
 const cardArray = [
     {
     name: "blue",
-    img: "img/blue-flower.png"
+    img: "./img/blue-flower.png"
 
 },
     {
     name: "blue",
-    img: "img/blue-flower.png"
+    img: "./img/blue-flower.png"
 
 },
 
     {
     name: "pink",
-    img: "img/pink-flower.png"
+    img: "./img/pink-flower.png"
 
 },
 
     {
     name: "pink",
-    img: "img/pink-flower.png"
+    img: "./img/pink-flower.png"
 
 },
 
     {
     name: "purple",
-    img: "img/purple-flower.png"
+    img: "./img/purple-flower.png"
 
 },
 
     {
     name: "purple",
-    img: "img/blue-flower.png"
+    img: "./img/blue-flower.png"
 
 },
 
     {
     name: "orange",
-    img: "img/orange-flower.png"
+    img: "./img/orange-flower.png"
 
 },
     {
     name: "orange",
-    img: "img/orange-flower.png"
+    img: "./img/orange-flower.png"
 
 },
     {
     name: "green",
-    img: "img/green-flower.png"
+    img: "./img/green-flower.png"
 
 },
     {
     name: "green",
-    img: "img/green-flower.png"
+    img: "./img/green-flower.png"
 
 },
     {
     name: "yellow",
-    img: "img/yellow-flower.png"
+    img: "./img/yellow-flower.png"
 
 },
     {
     name: "yellow",
-    img: "img/yellow-flower.png"
+    img: "./img/yellow-flower.png"
 
 }
 ]
 
+const grid = document.querySelector(".grid");
+
 //Creating board
 
-const grid = document.querySelector(.grid);
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement("img")
+
+        card.setAttribute("src", "../img/question-mark.png")
+
+        card.setAttribute(data-id, i)
+
+        //card.addEventListener("click", flipcard)
+
+        grid.appendChild(card)
+    }
+}
+
 
